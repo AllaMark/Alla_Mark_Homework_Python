83559a551bc70c0910c7543741fc22abc09890c7
AllaMark <allamark.rabota@gmail.com>
2026-03-31 14:12:31 +0400

    Домашнее задание 6

--- lesson_06/lesson06_task2.py
+++ lesson_06/lesson06_task2.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
 driver.get("http://uitestingplayground.com/textinput")
 
-element = driver.find_element(By.CSS_SELECTOR, "#newButtonName")
+element = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#newButtonName")))
 element.send_keys("SkyPro")
 
 button = driver.find_element(By.CSS_SELECTOR, "#updatingButton")
